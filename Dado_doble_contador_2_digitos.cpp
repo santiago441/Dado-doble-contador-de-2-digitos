e79f2269808bf3e857fcc3643be3266f44147d04
Santiago González Fagua <113556041+santiago441@users.noreply.github.com>
2022-09-17 16:29:19 -0500

    Update Dado_doble_contador_2_digitos.cpp

--- Dado_doble_contador_2_digitos.cpp
+++ Dado_doble_contador_2_digitos.cpp
@@ -2,28 +2,28 @@
 
 DigitalIn btn_1(A1);
 DigitalIn btn_2(A2);
-DigitalIn btn_3(BUTTON1);
+DigitalOut led_verde(A0);
+DigitalOut led_rojo(A5);
 
-BusOut Segmentos(D2,D3,D4,D5,D6,D7,D8,D9); // pines segmentos A B C D E F G DP
+BusOut Segmentos(D2,D3,D4,D5,D6,D7,D8,D9); 
 BusOut Display(A4,A3);
 
-const char secuencia[6] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF}; // A B C D E F
-const char numeros[6] = {0xF9,  0xB0, 0xA4, 0x99, 0x92, 0x82}; // 1 2 3 4 5 6
+const char numeros[7] = {0xC0/*0*/, 0xF9 /*1*/,  0xA4/*2*/, 0xB0/*3*/, 0x99/*4*/, 0x92/*5*/, 0x82/*6*/};
 
-char num_1; 
-char num_2; 
+int num_1, num_2; 
 
-void Efecto(void);
+
 void visualizar(void);
-void random(void);
 
-int main(){  
+int main(){   
 
-    while(true){
+    Segmentos = 0xC0;
 
-        }
+    visualizar();
 
-    }
+    while(true){ 
+
+    }   
 
 }
 
@@ -33,18 +33,63 @@ void visualizar(void){
 
     while(true){
 
+        if(Segmentos == 0xC0){
+
+            led_verde = 0;
+            led_rojo  = 0; 
+
+        }
+
         if(flag){
 
-            Display = 0b01;
+            if(btn_1){
+
+                srand(time(NULL));
+                num_1 = rand()%6+1;
+                ThisThread::sleep_for(500ms);
+                num_2 = 0xC0;
+                led_rojo  = 0;
+                led_verde = 0;
+
+            }
+
+            Display = 0b10;
             Segmentos = numeros[num_1];
+
         }
         else{
 
-            Display = 0b10;
-            Segmentos = numeros[num_2];
-        }
+            if(btn_2){
 
+                srand(time(NULL));
+                num_2 = rand()%6+1;
+            }
+
+            Display = 0b01;
+            Segmentos = numeros[num_2];
+
+        }
         flag = !flag;
-        ThisThread::sleep_for(11ms);
+        ThisThread::sleep_for(10ms);
+
+        
+
+        if(num_1 > num_2){
+
+            led_verde = 0;
+            led_rojo  = 1;
+
+        } 
+        else if(num_1 <= num_2){
+
+            led_rojo  = 0;
+            led_verde = 1;
+
+        }
+        
+       
+
     }
+
+
 }
