ad24430b60140267303d09ad9032ac904e8cdfc6
Santiago González Fagua <113556041+santiago441@users.noreply.github.com>
2022-09-20 11:50:11 -0500

    Update Dado_doble_contador_2_digitos.cpp

--- Dado_doble_contador_2_digitos.cpp
+++ Dado_doble_contador_2_digitos.cpp
@@ -1,43 +1,61 @@
+#include "BusOut.h"
 #include "mbed.h"
-#define tiempo_conteo  150
-#define tiempo_refresco  1
-#define tiempo  99999999        
+#include "Thread.h"
+
+#define TIEMPO_REFRESCO 11ms
+#define TIEMPO_CONTEO 1s
 
 DigitalIn btn_1(A1);
 DigitalIn btn_2(A2);
 DigitalOut led_verde(A0);
 DigitalOut led_rojo(A5);
+DigitalIn Switch(PC_8);
+DigitalIn btn1(BUTTON1);
 DigitalIn bit1(D10);
 DigitalIn bit2(D11);
-DigitalIn bit3(D12);
-DigitalIn bit4(D13);
-DigitalIn bit5(D14);
-DigitalIn bit6(D15);
-DigitalIn bit7(PB_12);
-DigitalIn botonup(PB_14);
-DigitalIn botondown(PB_15);
-static int unidad=0,x=0;
 
+BusIn  Disp_1 (D13,D12,D11,D10);
+BusIn  Disp_2 (PA_11,PB_12,D15,D14);
 BusOut Segmentos(D2,D3,D4,D5,D6,D7,D8,D9); 
 BusOut Display(A4,A3);
 
+const char numeros[10] = {0xC0/*0*/, 0xF9 /*1*/,  0xA4/*2*/, 0xB0/*3*/, 0x99/*4*/, 0x92/*5*/, 0x82/*6*/,0XF8/*7*/, 0x80/*8*/, 0x98/*9*/};
 
-const char numeros[7] = {0xC0/*0*/, 0xF9 /*1*/,  0xA4/*2*/, 0xB0/*3*/, 0x99/*4*/, 0x92/*5*/, 0x82/*6*/};
-const char segmentos [12] = {0xC0/*0*/, 0xF9 /*1*/,  0xA4/*2*/, 0xB0/*3*/, 0x99/*4*/, 0x92/*5*/,0x82/*6*/, 0x78/*7*/, 0x00/*8*/, 0x18/*9*/, 0xf9,/*1*/,0xFF/*6*/};
+static char unidades = 0;
+static char decenas  = 0;
+static bool flag_run = false;
 
-int num_1, num_2,numero,button1,button2;
-int a=0,b=0,c=0,d=0,e=0,f=0,g=0;
-int unidades=0,decenas=0,parpadeo=0;
-        
-void conteo(void);
+int num_1, num_2;
+
+Thread hilo_visualizar;
+Thread hilo_contar;
+Thread hilo_boton1;
+Thread hilo_boton2;
+Thread hilo_numero;
+
+
 void dado(void);
+void visualizar(void);
+void contar(void);
+void boton1(void);
+void boton2(void);
+void numero(void);
+
 
 int main(){   
 
     Segmentos = 0xC0;
 
-    dado();
-
+    if(Switch){
+        dado();
+    }
+    else{
+        hilo_visualizar.start(visualizar);
+        hilo_contar.start(contar);
+        hilo_boton1.start(boton1);
+        hilo_boton2.start(boton2);
+        hilo_numero.start(numero);
+    }
     while(true){ 
 
     }   
@@ -59,7 +77,7 @@ void dado(void){
 
         if(flag){
 
-            if(btn_1){
+            if(btn_1 && (!bit1 || !bit2)){
 
                 srand(time(NULL));
                 num_1 = rand()%6+1;
@@ -76,7 +94,7 @@ void dado(void){
         }
         else{
 
-            if(btn_2){
+            if(btn_2 && (bit1 || bit2)){
 
                 srand(time(NULL));
                 num_2 = rand()%6+1;
@@ -91,25 +109,25 @@ void dado(void){
 
         
 
-        if(num_1 > num_2 && num_2 != 0 && !Switch_1 && Switch_2){
+        if(num_1 > num_2 && num_2 != 0 && !bit1 && bit2){
 
             led_rojo  = 0;
             led_verde = 1;
 
         } 
-        else if(num_1 > num_2 && num_2 != 0 && Switch_1 && !Switch_2){
+        else if(num_1 > num_2 && num_2 != 0 && bit1 && !bit2){
 
             led_rojo  = 1;
             led_verde = 0;
 
         } 
-        else if(num_1 <= num_2 && num_2 != 0 &&  Switch_1 && !Switch_2){
+        else if(num_1 <= num_2 && num_2 != 0 &&  bit1 && !bit2){
 
             led_rojo  = 0;
             led_verde = 1;
 
         }
-        else if(num_1 <= num_2 && num_2 != 0 &&  !Switch_1 && Switch_2){
+        else if(num_1 <= num_2 && num_2 != 0 &&  !bit1 && bit2){
 
             led_rojo  = 1;
             led_verde = 0;
@@ -121,121 +139,125 @@ void dado(void){
 
 
 }
+void visualizar(void){
 
-void conteo (void)
-{
-    numero=bit5;
-    if (numero==false)
-    {
-        while (true)
-        {
-            if bit1==true{
-                a=1;
-            }else{
-                a=0;
-            }
-            if bit2==true{
-                b=2;
-            }else{
-                b=0;
-            }
-            if bit3==true{
-                c=4;
-            }else{
-                c=0;
-            }
-            if bit4==true{
-                d=8;
-            }else{
-                d=0;
-            }
-            if bit5==true{
-                e=16;
-            }else{
-                e=0;
-            }
-            if bit6==true{
-                f=32;
-            }else{
-                f=0;
-            }
-            if bit1==true{
-                g=64;
-            }else{
-                g=0;
-            }
-            unidad= a+b+c+d+f+e+g;
-            if (unidad>99) unidad = 99;
-                
-              decenas=(unidades/10);
-              unidades= (unidades-(decenas*10));
-            
-            button1=botonup.read();
-            if(button1==false)
-            {
-                while==true;
-                {
-                   unidades=unidades+1;
-                    if unidades==10
-                    {
-                        unidades=0;
-                        decenas=decenas+1:
-                        
-                        if decenas==10{
-                            while(true)
-                            {
-                                switch (parpadeo)
-                                {
-                                        case 0: decenas=9;
-                                               unidades=9;
-                                               parpadeo=1;
-                                               x=x+1;
-                                        break;
-                                        default: decenas=11;unidades=11; parpadeo=0; break;
-                                }
-                                 if (x==6)     
-                                 {
-                                   thread_sleep_for(tiempo);
-                            } 
-                            thread_sleep_fsor(tiempo_conteo);
-                        }
-                    }
-                }          
-                        thread_sleep_for(tiempo_conteo);
-                        }
-                }    
-                 button2=botondown.read();
-                if (botondown == false)
-                 {
-                  while(true)
-             {
-                if (unidades==0)
-                {
-                    unidades=10;
-                    if (decenenas==0)
-                    {
-                        while(true)
-                        { 
-                            switch  (parpadeo)
-                            { 
-                            case 0:  decenas=10;unidades=10; parpadeo=1; x++; break;
-                            default: decenas=11;unidades=11; parpadeo=0; break;
-                            }
-                            if (x== 6)
-                            {
-                                thread_sleep_for(tiempo);
-                            } 
-                            thread_sleep_for(tiempo_conteo);
-                        }
-                    }
-                    decenas=decenas-1;
-                }
-                unidades=unidades-1;
-            thread_sleep_for(tiempo_conteo);
-            }
-       }
-    }  
+    bool flag;
+
+    while(true){
+
+        if(flag){
+
+            Display = 0b01;
+            Segmentos = numeros[unidades];
+
+        }
+        else{
+
+            Display = 0b10;
+            Segmentos = numeros[decenas];
+        }
+        flag = !flag;
+        ThisThread::sleep_for(TIEMPO_REFRESCO);
+
     }
+    
 }
-                
+
+void contar(void){
+
+    while(true){
+
+        if(flag_run){
+
+            unidades++;
+
+        }
+        if(unidades==10){
+
+            unidades=0;
+            decenas++;
+
+            if(decenas==10) decenas=0;
+
+        }
         
+        
+     ThisThread::sleep_for(TIEMPO_CONTEO);
+
+    }
+
+}
+
+void boton1(void){
+
+    bool Q0,Q1,Q2;
+
+    while(true){
+
+        Q2=Q1;
+        Q1=Q0;
+        Q0=btn1;
+
+        if(!Q0 && Q1 && Q2){
+
+            flag_run = true;
+  
+        }
+
+        ThisThread::sleep_for(50ms);
+
+    }
+    
+}
+
+void boton2(void){
+
+    bool Q0,Q1,Q2;
+
+    while(true){
+
+        Q2=Q1;
+        Q1=Q0;
+        Q0=btn_2;
+
+        if(!Q0 && Q1 && Q2){
+
+            flag_run = false;
+  
+        }
+
+        ThisThread::sleep_for(50ms);
+
+    }
+    
+}
+
+void numero(void){
+
+    if(!flag_run){
+
+    if(Disp_1==0x00){decenas = 0;}
+    else if(Disp_1==0x01){decenas = 1;}
+    else if(Disp_1==0x02){decenas = 2;}
+    else if(Disp_1==0x03){decenas = 3;}
+    else if(Disp_1==0x04){decenas = 4;}
+    else if(Disp_1==0x05){decenas = 5;}
+    else if(Disp_1==0x06){decenas = 6;}
+    else if(Disp_1==0x07){decenas = 7;}
+    else if(Disp_1==0x08){decenas = 8;}
+    else if(Disp_1==0x09){decenas = 9;}
+
+    if(Disp_2==0x00){unidades = 0;}
+    else if(Disp_2==0x01){unidades = 1;}
+    else if(Disp_2==0x02){unidades = 2;}
+    else if(Disp_2==0x03){unidades = 3;}
+    else if(Disp_2==0x04){unidades = 4;}
+    else if(Disp_2==0x05){unidades = 5;}
+    else if(Disp_2==0x06){unidades = 6;}
+    else if(Disp_2==0x07){unidades = 7;}
+    else if(Disp_2==0x08){unidades = 8;}
+    else if(Disp_2==0x09){unidades = 9;}
+
+    }
+
+}
